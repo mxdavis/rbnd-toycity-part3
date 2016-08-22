9cb638ba0f0b1abe0b03be155a09c59e68b186aa
Malki Davis <md01@me.com>
2016-08-23 01:56:36 +0300

    adding products only if product title does not already exist in the product array

--- lib/product.rb
+++ lib/product.rb
@@ -1,11 +1,26 @@
 class Product
-	@@products = []
+  attr_reader :title
 
-	def initialize(options={})
-		@@products << self
-	end
+  @@products = []
 
-	def self.all
-		@@products
-	end
+  def initialize(options={})
+    @title = options[:title]
+    add_to_products
+  end
+
+  def self.all
+    @@products
+  end
+
+  private
+
+   def add_to_products
+    unique_name = @@products.map {|product| product.title}
+    if !unique_name.include? self.title
+      @@products << self
+    else
+      raise DuplicateProductError, "#{self.title} already exists"
+    end
+  end
 end
+
