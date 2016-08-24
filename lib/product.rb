6cd93c752c943922cf5441300abb9c3966d37029
Malki Davis <md01@me.com>
2016-08-24 09:57:43 +0300

    create self.all, self.find_by_title and in_stock?

--- lib/product.rb
+++ lib/product.rb
@@ -1,15 +1,29 @@
 class Product
-  attr_reader :title
+  attr_reader :title, :price, :stock
 
   @@products = []
 
   def initialize(options={})
     @title = options[:title]
+    @price = options[:price]
+    @stock = options[:stock]
     add_to_products
   end
 
   def self.all
     @@products
+  end
+
+  def self.find_by_title(title_name)
+    @@products.find {|product| product.title == title_name}
+  end
+
+  def in_stock?
+    if @stock > 0
+      return true
+    else
+      return false
+    end
   end
 
   private
