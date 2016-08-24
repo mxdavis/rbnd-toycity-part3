7dd7f42074ce288614e62c6a75e7538035c1f3e3
Malki Davis <md01@me.com>
2016-08-24 10:20:28 +0300

    add customer class and applicable methods

---
+++ lib/customer.rb
@@ -0,0 +1,29 @@
+class Customer
+  attr_accessor :name
+
+  @@customers = []
+
+  def initialize(options={})
+   @name = options[:name]
+   add_to_customers
+  end
+
+  def self.all
+    @@customers
+  end
+
+  def self.find_by_name(customer_name)
+    @@customers.find {|customer| customer.name == customer_name}
+  end
+
+  private
+
+   def add_to_customers
+    unique_name = @@customers.map {|customer| customer.name}
+    if !unique_name.include? self.name
+      @@customers << self
+    else
+      raise DuplicateProductError, "#{self.name} already exists"
+    end
+  end
+end
